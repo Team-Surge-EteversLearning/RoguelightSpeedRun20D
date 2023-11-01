a5a4074fa2557619a6b5a9642c135b48b63bc5ef
seintcat <1x2x0x7@naver.com>
2023-11-01 21:50:14 +0900

    설계도 작성 및 기능 테스트중

---
+++ RoguelightSpeedRun20D/Assets/_/Seintcat/SeintcatTestCode.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeintcatTestCode : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+public interface II { }

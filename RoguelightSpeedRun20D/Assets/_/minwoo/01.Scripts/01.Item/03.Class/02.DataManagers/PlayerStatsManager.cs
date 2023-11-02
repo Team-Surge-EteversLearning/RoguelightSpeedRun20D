fe125be6118147e6eb9ce589ba487a7873f35abe
minwoo00044 <70777372+minwoo00044@users.noreply.github.com>
2023-11-02 12:58:16 +0900

    usable item, stat shop
    EquipmentBasicData.cs
    ShopProduct.cs
    Armor.cs
    Equipment.cs
    Shoes.cs
    Weapon.cs
    Useable.cs
    DungeonItemManager.cs
    EquipmentDataManager.cs
    그외 스탯.cs

---
+++ RoguelightSpeedRun20D/Assets/_/minwoo/01.Scripts/01.Item/03.Class/02.DataManagers/PlayerStatsManager.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class PlayerStatsManager : IProductMaker
+{
+    static string name;
+    static int cashNow;
+
+    static int hpMax;
+    static int staminaMax;
+    static int manaMax;
+    static float speed;
+    static float powerWeight;
+
+    static int hpMaxPrice;
+    static int staminaMaxPrice;
+    static int manaMaxPrice;
+    static int speedPrice;
+    static int powerWeightPrice;
+    //if buy() stat, Price is added
+
+    static List<IProduct> stats;
+    static Dictionary<IProduct, int> priceTable = new Dictionary<IProduct, int>();
+
+    void Init()
+    {
+        //read PlayerData and it's stored in the variable
+        stats = new List<IProduct>()
+        {
+            //Since each statProduct has only one type and does not have different information,
+            //it was judged that it was okay to generate it every time.
+            new MaxHp(), new MaxStamina(),new MaxStamina(), new PowerWeight(), new Speed()
+        };
+        priceTable.Add(stats[0], hpMaxPrice);
+        priceTable.Add(stats[1], staminaMaxPrice);
+        priceTable.Add(stats[2], manaMaxPrice);
+        priceTable.Add(stats[3], speedPrice);
+        priceTable.Add(stats[4], powerWeightPrice);
+    }
+
+    public void AddPrice(int index, int weightValue)
+    {
+        priceTable[stats[index]] += weightValue;
+    }
+
+    public List<ShopProduct> Make(string info)
+    {
+        List<ShopProduct> displayItemListWithPrice = new List<ShopProduct>();
+
+        for (int i = 0; i < 5; i++)
+        {
+            IProduct newStat = stats[i];
+            ShopProduct product = new ShopProduct(newStat, priceTable[newStat]);
+            displayItemListWithPrice.Add(product);
+        }
+        //return to shop
+        return displayItemListWithPrice;
+    }
+}
+

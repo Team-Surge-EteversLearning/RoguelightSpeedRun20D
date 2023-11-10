1e1b8b5de786f9cf045c57cd957f44f9dcfab68a
bestwondev <bestwondev@gmail.com>
2023-11-10 13:48:44 +0900

    근접 공격 마법사 몬스터 업데이트

---
+++ RoguelightSpeedRun20D/Assets/_/choiseungwon/FantasyMonster/WizaradMonsterLonDistance.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Don't edit this, Copy this!
+public class WizaradMonsterLonDistance : MonsterSM
+{
+    protected override List<State> monsterBattleStates => new List<State>();
+
+    private void Awake()
+    {
+        ManagerStart();
+    }
+
+    public override void MakeState()
+    {
+        // make custom states
+
+        // make basic states
+        base.MakeState();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    protected override void _FixedUpdate()
+    {
+
+    }
+
+    public override void TargetChanged(List<GameObject> target)
+    {
+        
+    }
+
+    public override void Interrupt(string stateName)
+    {
+        // not using
+    }
+
+    protected override void OnTriggerEnter(Collider other)
+    {
+        AttackAble otherAttack = other.GetComponent<AttackAble>();
+        if (otherAttack != null)
+        {
+            GetDamage(otherAttack.GetDamage(gameObject), other);
+            if (mainBody.Raycast(new Ray(other.transform.position, other.transform.forward), out RaycastHit hit, 0.1f))
+            {
+                StateManager stateManager = hit.collider.gameObject.GetComponent<StateManager>();
+                if (stateManager != null)
+                    attackTarget = stateManager.gameObject;
+            }
+        }
+    }
+
+    protected override void ReactDamage()
+    {
+        
+    }
+
+    protected override void ResetStateMachine()
+    {
+        
+    }
+}
+
+/*
+ * other code
+ * ChangeState("name");
+ * monsterIdleState.stateName = "Idle"
+ * monsterDamageState.stateName = "Damage"
+ * monsterDeathState.stateName = "Death"
+ * monsterPatrolState.stateName = "Patrol"
+ */
